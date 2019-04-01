191dd695e8b99dc71f04489b61438726f72b8d49
Johnny Toxin <thejohnnytoxin@gmail.com>
2019-03-31 20:18:30 -0400

    Add random seed generation at app start

--- PasswordGenerator/PasswordGenerator/PasswordGenerator.cpp
+++ PasswordGenerator/PasswordGenerator/PasswordGenerator.cpp
@@ -9,6 +9,8 @@ int main(int argc, char* argv[])
 {
     SetConsoleTitle(L"Johnny Toxin's Password Generator v1.0.0.0");
 
+    // Initialize random with seed
+    srand(GetTickCount());
     Generator generator;
     // Get the executable path
     // Source: http://www.cplusplus.com/forum/windows/49104/#msg266992
